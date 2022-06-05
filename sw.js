2c211f509805b3102873d50b1629d33d925f631b
vdabrowska <damaris.dabrowska@gmail.com>
2022-06-05 10:11:00 +0200

    slightly changed sw.js and background color again

--- sw.js
+++ sw.js
@@ -1,26 +1,30 @@
 var cacheName = "podyplomowe";
 var filesToCache = ["./", "./index.html", "./css/style.css", "./js/main.js"];
 
-/* Start the service worker and cache all of the app's content */
-self.addEventListener("install", function (event) {
-// Perform install steps
-event.waitUntil(
-    caches.open(cacheName).then(function (cache) {
-    console.log("Opened cache");
-    return cache.addAll(filesToCache);
-    })
-);
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    console.log('installing sw');
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                var x = cache.addAll(urlsToCache);
+                console.log('cache added');
+                return x;
+            })
+    );
 });
 
-/* Serve cached content when offline */
-self.addEventListener("fetch", function (event) {
-event.respondWith(
-    caches.match(event.request).then(function (response) {
-    // Cache hit - return response
-    if (response) {
-        return response;
-    }
-    return fetch(event.request);
-    })
-);
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                    // Cache hit - return response
+                    if (response) {
+                        return response;
+                    }
+                    return fetch(event.request);
+                }
+            )
+    );
 });
